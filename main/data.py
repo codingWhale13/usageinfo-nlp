72f661bf1082bc9f4f80cf0879e098f3f76c5b37
FSadrieh <Frederic.Sadrieh@student.hpi.uni-potsdam.de>
2022-10-13 15:29:50 +0200

    Added conversion to utf-8

--- main/data.py
+++ main/data.py
@@ -8,14 +8,22 @@ for link in links.links:
     file_name=link.split('/')[-1][:-3]
     file_location=f'data/{file_name}'
     if not os.path.exists(file_location):
-        url=f'https://s3.amazonaws.com/amazon-reviews-pds/tsv/{file_name}.gz'
-        file = wget.download(url, f'{file_location}.gz')
+        file = wget.download(link, f'{file_location}.gz')
 
         with gzip.open(f'{file_location}.gz', 'rb') as f_in:
-            with open(file_location, 'wb') as f_out:
+            with open(f'data/temp_{file_name}', 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
+
+        with open(f'data/temp_{file_name}', 'r') as inp:
+            with open(file_location, 'w') as outp:
+                for line in inp:
+                    line=bytes(line, 'cp1252').decode('utf-8','ignore')
+                    outp.write(line)
+
         try:
             os.remove(f'{file_location}.gz')
+            os.remove(f'data/temp_{file_name}')
             os.remove('data/*.tmp')
         except:
+            print('Error removing files')
             pass
