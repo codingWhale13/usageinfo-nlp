c0dd2b8722eb2da3ae645ff0374deaf351960449
Konstantin Ketterer <konstantin.ketterer@d-labs.com>
2022-11-08 18:02:50 +0100

    Style CustomUsageOptionFormTag

--- labellingTool/src/components/UsageOptionTag.jsx
+++ labellingTool/src/components/UsageOptionTag.jsx
@@ -28,7 +28,9 @@ export function CustomUsageOptionFormTag({onSave}){
     const saveNewCustomUsageOption = (e) => {
         e.preventDefault();
         const newCustomUsageOption = e.target.custom_usage_option.value;
-        onSave(newCustomUsageOption);
+        if(newCustomUsageOption){
+            onSave(newCustomUsageOption);
+        }
         //Reset form input
         e.target.custom_usage_option.value= '';
     };
@@ -37,15 +39,15 @@ export function CustomUsageOptionFormTag({onSave}){
 
     const formRef = React.createRef();
 
-    return <Tag>
+    return <Tag p={0}>
         <form onSubmit={saveNewCustomUsageOption} ref={formRef}>
             <InputGroup size='md'>
             <Input
                 name='custom_usage_option'
                 placeholder='Add new usage option'
             />
-            <InputRightAddon children={
-                   <Button type='submit' m={0} p={0} spacing={0}> < AddIcon /></Button>
+            <InputRightAddon  p={0} children={
+                   <Button type='submit' > < AddIcon /></Button>
             }/>
              
             </InputGroup>
