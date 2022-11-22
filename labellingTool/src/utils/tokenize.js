1c54b4a5bcf5076dc36b54595e7f8defa0522c68
codingWhale13 <nils.kiele@gmail.com>
2022-11-23 00:15:38 +0100

    labellingTool [Feature] Make displayed text more readable

--- labellingTool/src/utils/tokenize.js
+++ labellingTool/src/utils/tokenize.js
@@ -1,5 +1,13 @@
-function tokenizeString(string){
-    return string.replace(/[^\w\s]|_/g, function ($1) { return ' ' + $1 + ' ';}).replace(/[ ]+/g, ' ').split(' ');
+function tokenizeString(string) {
+  return string
+    .replaceAll('<br />', '') // remove new lines (\n doesn't work, sadly...)
+    .replaceAll('\\', '') // these guys are sometimes in the text
+    .replaceAll('&#34;', '"') // show quotes
+    .replaceAll(/[^\w\s-']|_/g, function ($1) {
+      return ' ' + $1 + ' ';
+    })
+    .replaceAll(/[ ]+/g, ' ') // replace multiple spaces with single space
+    .split(' ');
 }
 
 module.exports = tokenizeString;
