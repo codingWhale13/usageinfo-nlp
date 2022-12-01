901a144b9568af14b142ea389f8ca19d5e84e1bc
codingWhale13 <nils.kiele@gmail.com>
2022-12-01 18:29:57 +0100

    labellingTool [fix]: remove <BR> tags

--- labellingTool/src/utils/tokenize.js
+++ labellingTool/src/utils/tokenize.js
@@ -1,6 +1,7 @@
 function tokenizeString(string) {
   return string
     .replaceAll('<br />', '') // remove new lines (\n doesn't work, sadly...)
+    .replaceAll('<BR>', '') // remove new lines (\n doesn't work, sadly...)
     .replaceAll('\\', '') // these guys are sometimes in the text
     .replaceAll('&#34;', '"') // show quotes
     .replaceAll(/[^\w\s-']|_/g, function ($1) {
