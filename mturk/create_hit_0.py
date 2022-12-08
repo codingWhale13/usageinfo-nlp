364af21e0f6eba79f395b605e3b3fefc3033b8d3
Konstantin Ketterer <konstantin.ketterer@d-labs.com>
2022-12-08 19:01:15 +0100

    Add create hit turk api

---
+++ mturk/create_hit_0.py
@@ -0,0 +1,11 @@
+import boto3
+client = boto3.client('mturk') # endpoint_url='https://mturk-requester-sandbox.us-east-1.amazonaws.com'
+with open('question.xml', 'r') as f:
+    param = f.read()
+response = client.create_hit_with_hit_type(
+    HITTypeId="3BKZR1PB4UPWCDE3BFF22499LOQ6KB",
+    MaxAssignments=2,
+    LifetimeInSeconds=3600,
+    Question=param,
+    UniqueRequestToken='bsc_usage_option_test_1'
+)
--- mturk/sample_tsv_to_manifest.py
+++ mturk/sample_tsv_to_manifest.py
@@ -2,10 +2,11 @@
 import pandas as pd
 import json
 import argparse
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--file', '-f', required=True, help="Sample file without .tsv extension")
 parser.add_argument('--number_of_reviews', '-n', help="The number of reviews in the output manifest. Shuffled randomly")
+parser.add_argument('--reviews_per_task', '-b', default= 5, type=int, help="The number of reviews for each task", )
+
 
 args = parser.parse_args()
 tsv_file = args.file
@@ -25,26 +26,47 @@ if args.number_of_reviews is not None:
         total_output_reviews = number_of_reviews
     random.shuffle(json_df)
 # %%%
+from itertools import zip_longest
+
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
 
 manifest = []
 SOURCE_COLUMN = 'review_body'
 METADATA_COLUMNS = ['review_id', 'product_id', 'product_title', 'review_date', 'product_category']
-for i, review in enumerate(json_df[:total_output_reviews]):
-    metadata = review.copy()
-    del metadata['review_body']
-    datapoint = {
-        "source" : review[SOURCE_COLUMN],
-        "metadata" : {
-            'total_reviews' : total_output_reviews,
-            'current_review_index' : i
-        }
-    }
-    for column in METADATA_COLUMNS:
-        datapoint['metadata'][column] = review[column]
 
-    manifest.append(datapoint)
+reviews_per_task = args.reviews_per_task
+
+for reviews_batch in grouper(json_df[:total_output_reviews], reviews_per_task):
+    source = []
+    metadata = []
+    for review in reviews_batch:
+        review_metadata = review.copy()
+        del review_metadata['review_body']
+        source.append(review[SOURCE_COLUMN])
+        
+        datapoint_metadata = {}
+        for column in METADATA_COLUMNS:
+            datapoint_metadata[column] = review[column]
+        metadata.append(datapoint_metadata)
+
+    manifest.append({
+    "source" : source,
+    "metadata" : metadata
+   })
 
 manifest
+
+# %%
+from create_hit import create_hit_question_xml
+with open('question.xml','w') as file:
+    source = json.dumps(manifest[0]["source"])
+    metadata = json.dumps(manifest[0]['metadata'])
+    xml = create_hit_question_xml(source, metadata)
+    file.write(str(xml))
 # %%
 import time
 json_string_dataset = [json.dumps(row, ensure_ascii=False) for row in manifest]
