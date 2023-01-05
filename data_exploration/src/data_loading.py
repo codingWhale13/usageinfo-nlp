273c76588562a84097dab00bc719e1de90c9fcd2
Matthis Clausen <matthis.clausen@student.hpi.uni-potsdam.de>
2023-01-05 17:44:25 +0100

    [matthis/extract-json-from-manifest]: [matthis/extract-json-from-manifest]: add function to extract json in form of label tool out of manifest output
    * make output_path optional
    
    * extract function for determining run-name
    
    * add function to extract json in form of label tool out of manifest output
    
    
    Merged by: Matthis Clausen <matthis.clausen@student.hpi.uni-potsdam.de>
    Co-authored-by: Matthis Clausen <matthis.clausen@student.hpi.de>

--- data_exploration/src/data_loading.py
+++ data_exploration/src/data_loading.py
@@ -158,3 +158,37 @@ def filter_reviews_by_word_count(
     df = df[df["review_body"].str.split(" ").str.len() > min_word_count]
     df = df[df["review_body"].str.split(" ").str.len() < max_word_count]
     return df
+
+def extract_json_from_manifest(input_path: Union[Path, str], output_path: Union[Path, str] = None):
+    with open(input_path, "r") as turker_labels:
+        reviews = {"reviews": [], "maxReviewIndex": 0}
+        run_name = get_run_name(turker_labels)
+        if output_path is None:
+            output_path = f"{run_name}-output.json"
+        for line in turker_labels:
+            data = json.loads(line)
+            review_bodies = json.loads(data["source"])
+            number_of_workers_per_hit = len(data[run_name]["annotationsFromAllWorkers"])
+            number_of_reviews_per_hit = len(review_bodies)
+            for worker in range(number_of_workers_per_hit):
+                for review in range(number_of_reviews_per_hit):
+                    inner_annotations = json.loads(data[run_name]["annotationsFromAllWorkers"][worker]["annotationData"]["content"])
+                    annotations = json.loads(inner_annotations["annotations"])["annotations"]
+                    customUsageOptions = json.loads(inner_annotations["annotations"])["customUsageOptions"]
+                    reviews["reviews"].append(data["metadata"][review] | {
+                    "review_body": review_bodies[review],
+                    "label": {
+                        "isFlagged": False,
+                        "annotations": annotations[review],
+                        "customUsageOptions": customUsageOptions[review],
+                        "replacementClasses": {}
+                    },
+                    "inspectionTime": None})
+        with open(output_path, "w") as output_file:
+            json.dump(reviews, output_file)
+
+def get_run_name(turker_labels):
+    data = json.loads(turker_labels.readline())
+    for key, value in data.items():
+        if isinstance(value, dict) and "job-name" in value:
+            return data[key]["job-name"]
