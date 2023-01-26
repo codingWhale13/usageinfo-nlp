a81c3bf8abfb192fd7480bfbfc5b4cc1472c0e7e
Konstantin Ketterer <konstantin.ketterer@d-labs.com>
2023-01-26 12:07:28 +0100

    Support ratings deployment

--- labellingTool/src/featureFlags.js
+++ labellingTool/src/featureFlags.js
@@ -1,8 +1,9 @@
 export function getFeatureFlags() {
   const mTurk = process.env.REACT_APP_MTURK === '1';
+  const rate =  process.env.REACT_APP_RATE === '1';
   return {
     negativeUseCases: false,
-    ratePredictedUseCases: true,
+    ratePredictedUseCases: rate,
     reviewLabelling: false,
     localLabelling: !mTurk,
     mTurk: mTurk,
