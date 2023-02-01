d605aee5595351d8c5f993370debfb3f5192e3fe
Konstantin Ketterer <konstantin.ketterer@d-labs.com>
2023-02-01 18:48:17 +0100

    Remove local amplify config

--- labellingTool2/pages/index.js
+++ labellingTool2/pages/index.js
@@ -1,10 +1,7 @@
-import 'core-js/actual/string/pad-end';
 import React from 'react';
 import { ChakraProvider, Container } from '@chakra-ui/react';
-import { Labeller } from '../components/Labeller';
 import { FlagsProvider } from 'flagged';
 import { getFeatureFlags } from '../featureFlags';
-import MTurkReview from '../components/MTurkReview';
 
 function App() {
   const features = getFeatureFlags();
@@ -12,11 +9,7 @@ function App() {
     <FlagsProvider features={getFeatureFlags()}>
       <ChakraProvider>
        <Container maxWidth="1300px" p='3'>
-        {features.mTurk ? (
-            <MTurkReview />
-        ) : (
-            <Labeller />
-        )}
+          gas
         </Container>
       </ChakraProvider>
     </FlagsProvider>
