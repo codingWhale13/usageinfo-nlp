b7eccf19a5adaf42461b9254d969ddf874331eee
Leonard Horns <leonard.horns@student.hpi.uni-potsdam.de>
2023-02-02 13:25:55 +0100

    [lh/create-dataset]: Add option to specify usage split when creating dataset
    * create_dataset [feat]: Add option to specify usage split
    
    (percentage of reviews containing at least one usage option, if not specified original split is used)
    
    
    Merged by: Frederic Sadrieh <frederic.sadrieh@student.hpi.uni-potsdam.de>
    Co-authored-by: lhorns <leonard.horns@student.hpi.de>

--- utils/create_dataset.py
+++ utils/create_dataset.py
@@ -32,6 +32,13 @@ def arg_parse():
         help="Percentage of the data to be used for testing",
     )
     parser.add_argument(
+        "-u",
+        "--usage-split",
+        type=float,
+        default=None,
+        help="Percentage of the data containing usage options",
+    )
+    parser.add_argument(
         "-s",
         "--seed",
         type=int,
@@ -59,13 +66,41 @@ def create_dataset_dir(name: str):
     return dataset_dir
 
 
-def create_dataset(files: List[str], test_split: float):
+def split_usageinfo_reviews(reviews):
+    reviews_with_usage, reviews_without_usage = [], []
+    for review in reviews:
+        label = review["label"]
+        if label["annotations"] or label["customUsageOptions"]:
+            reviews_with_usage.append(review)
+        else:
+            reviews_without_usage.append(review)
+    return reviews_with_usage, reviews_without_usage
+
+
+def filter_merge_reviews(list1, list2, split):
+    target_number = round((len(list1) / split) * (1 - split))
+    random.shuffle(list2)
+    return list1 + list2[:target_number]
+
+
+def create_dataset(files: List[str], usage_split, test_split: float):
     train_data = {"reviews": [], "maxReviewIndex": 0}
     test_data = {"reviews": [], "maxReviewIndex": 0}
     result = list()
     for file in files:
         with open(file, "r") as f:
             result.extend(json.load(f)["reviews"])
+
+    reviews_with_usage, reviews_without_usage = split_usageinfo_reviews(result)
+
+    result = (
+        filter_merge_reviews(reviews_with_usage, reviews_without_usage, usage_split)
+        if len(reviews_with_usage) / len(result) <= usage_split
+        else filter_merge_reviews(
+            reviews_without_usage, reviews_with_usage, 1 - usage_split
+        )
+    )
+
     random.shuffle(result)
     split_index = int(len(result) * (1 - test_split))
     train_data["reviews"] = result[:split_index]
@@ -74,9 +109,10 @@ def create_dataset(files: List[str], test_split: float):
     return train_data, test_data
 
 
-def create_yml(dataset_version, test_split, files, dataset_dir):
+def create_yml(dataset_version, usage_split, test_split, files, dataset_dir):
     dict_args = {
         "version": dataset_version,
+        "usage_split": usage_split if usage_split else "original",
         "test_split": test_split,
         "files": files,
     }
@@ -88,13 +124,16 @@ def main():
     args, _ = arg_parse()
     files = get_all_files(args.files)
     dataset_version = args.dataset_name
+    usage_split = args.usage_split
     test_split = args.test_split
     seed = args.seed
 
     random.seed(seed)
 
     dataset_dir = create_dataset_dir(name=dataset_version)
-    train_data, test_data = create_dataset(files=files, test_split=test_split)
+    train_data, test_data = create_dataset(
+        files=files, usage_split=usage_split, test_split=test_split
+    )
     with open(os.path.join(dataset_dir, "train_data.json"), "w") as output_file:
         json.dump(train_data, output_file)
 
@@ -103,6 +142,7 @@ def main():
 
     create_yml(
         dataset_version=dataset_version,
+        usage_split=usage_split,
         test_split=test_split,
         files=files,
         dataset_dir=dataset_dir,
