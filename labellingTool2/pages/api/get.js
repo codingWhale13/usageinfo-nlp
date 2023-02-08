ac9c64afa9c87a12d2cd68a087e9250d13bbe331
Konstantin Ketterer <konstantin.ketterer@d-labs.com>
2023-02-08 18:10:58 +0100

    Remove un used api routes

--- labellingTool2/pages/api/get.js
+++
@@ -1,7 +0,0 @@
-import { getAndReadStringObject } from "../../utils/aws/s3";
-
-export default async function handler(req, res) {
-    const data = await getAndReadStringObject('test/test_file');
-    console.log(data);
-    return data;
-};
--- labellingTool2/pages/api/list.js
+++
@@ -1,7 +0,0 @@
-import { listObjects } from "../../utils/aws/s3";
-
-export default async function handler(req, res) {
-    const data = await listObjects('test');
-    console.log(data);
-    return data;
-};
