c70f4c111b028e8d33e7cef853084d4006f9f2db
Nils Kiele <nils.kiele@student.hpi.uni-potsdam.de>
2023-03-10 15:45:26 +0100

    [nils/clarify-python-imports]: setup [refactor]: Update setup.py
    Probleme mit Python-Imports in der Evaluation sollen hiermit behoben sein.
    
    
    * setup [refactor]: Update setup.py
    
    
    Merged by: Frederic Sadrieh <frederic.sadrieh@student.hpi.uni-potsdam.de>
    Co-authored-by: codingWhale13 <nils.kiele@gmail.com>

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
-import os
 from setuptools import find_packages, setup
 
-print("WHEN IMPORTS DON'T WORK, EXECUTE THE FOLLOWING COMMAND FROM THE PROJECT FOLDER:")
-print("python -m pip install -e .")
-print("python3 -m pip install -e .")
-print(f'export PYTHONPATH="${{PYTHONPATH}}:{os.path.dirname(__file__)}"')
 
+# If you run into problems with Python imports, run the following command:
+# python -m pip install -e .
+# The command will use this setup.py file to do some magic.
 
 # https://stackoverflow.com/a/72124527
 setup(name="bsc2022-usage-info", packages=find_packages())
