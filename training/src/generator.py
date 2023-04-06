Fix: softmax before logging probs

training/src/generator.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 from typing import List
 from typing import Union
 
@@ -14,7 +15,7 @@ class Generator:
         generation_config: dict,
     ) -> None:
         self.model_artifact = {"name": artifact_name, "checkpoint": checkpoint}
-        checkpoint = torch.load(utils.get_model_path(self.model_artifact))
+        checkpoint = torch.load(utils.get_model_path(self.model_artifact), map_location=torch.device('cpu'))
         model_config = utils.get_model_config_from_checkpoint(
             checkpoint["model"], checkpoint
         )
@@ -37,38 +38,37 @@ class Generator:
         )
 
         outputs = self.model.generate(**batch[0], **self.generation_config)
-        print(outputs)
         predictions = self.tokenizer.batch_decode(outputs["sequences"], skip_special_tokens=True)
-        print(predictions)
         predictions = [
             self.format_usage_options(usage_options) for usage_options in predictions
         ]
         logprobs = self.get_logprobs(outputs)
+        print(logprobs)
         return zip(review_ids, model_inputs, predictions, logprobs)
     
     def get_logprobs(self, outputs):
+        # outputs["sequences"] are currently token ids, we need to decode every token. We slice off the first token because it is a pad token
+        # (Könnte bomben if we use something else than T5 later)
         predicted_tokens = [[self.tokenizer.decode(token) for token in review[1:]] for review in outputs["sequences"]]
-        token_probs = [[outputs["scores"][token_number][review_number, token] for token_number, token in enumerate(review[1:])] for review_number, review in enumerate(outputs["sequences"])]
+
+        # Use softmax to map to actual scores
+        probs = [F.softmax(scores, dim=1) for scores in outputs["scores"]]
+
+        # token_probs has review_number many entries, for every review we go through the relevant tokens and log their probability
+        token_probs = [[probs[token_number][review_number, token].item() for token_number, token in enumerate(review[1:])] for review_number, review in enumerate(outputs["sequences"])]
         
+        # outputs["sequences"] contains a lot of pad tokens, depending on the longest prediction in the batch (all predictions have equal length). We slice
+        # predicted_tokens and token_probs as soon as we see a pad token to only log relevant tokens
         for i in range(len(predicted_tokens)):
             for j in range(len(predicted_tokens[i])):
                 if predicted_tokens[i][j] == "<pad>":
                     predicted_tokens[i] = predicted_tokens[i][:j]
                     token_probs[i] = token_probs[i][:j]
                     break
-        print(predicted_tokens)
-        print(token_probs)
 
+        batch_probs = [{"predicted_tokens": predicted_tokens[i], "token_probs": token_probs[i]} for i in range(len(predicted_tokens))]
 
-        
-
-
-
-
-
-    
-
-
+        return batch_probs
 
 
     def generate_label(
@@ -86,28 +86,32 @@ class Generator:
             model_max_length=self.max_length,
             for_training=False,
         )
-        label_metadata = {
-            "generator": {
-                "artifact_name": self.model_artifact["name"],
-                "checkpoint": self.model_artifact["checkpoint"] or "last",
-                "generation_config": self.generation_config,
-            }
-        }
+
 
         if verbose:
             print(f"Generating label {label_id}...")
-            print(f"Label Metadata: {label_metadata}", end="\n\n")
 
         for batch in dataloader:
             usage_options_batch = self.generate_usage_options(batch)
             for review_id, model_input, usage_options, logprobs in usage_options_batch:
+
+                label_metadata = {
+                "generator": {
+                "artifact_name": self.model_artifact["name"],
+                "checkpoint": self.model_artifact["checkpoint"] or "last",
+                "generation_config": self.generation_config,
+                    }
+                }
+
                 if label_id is not None:
+                    label_metadata.update({"logprobs": logprobs})
                     reviews[review_id].add_label(
                         label_id=label_id,
                         usage_options=usage_options,
-                        metadata=label_metadata.update({"logprobs": logprobs}),
+                        metadata=label_metadata,
                     )
                 if verbose:
                     print(f"Review {review_id}")
                     print(f"Model input:\n{model_input}")
                     print(f"Usage options:\n\t{usage_options}", end="\n\n")
+                    print(f"Label Metadata: {label_metadata}", end="\n\n")