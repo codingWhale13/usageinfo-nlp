c3f72d59fba65da1fe16c536a5ae4d2a929d0878
Konstantin Ketterer <konstantin.ketterer@d-labs.com>
2023-06-17 21:25:42 +0200

    Generation [feat]: Add priority queue

--- training/generator.py
+++ training/generator.py
@@ -18,6 +18,12 @@ GENERATION_CONFIGS = [
         os.path.dirname(os.path.realpath(__file__)) + "/generation_configs/"
     )
 ]
+from queue import PriorityQueue
+from training import utils
+from helpers.review_set import ReviewSet
+from math import exp
+
+QUEUE_PROBABILITY_OFFSET = 1000_000_000
 
 
 class Generator:
@@ -89,7 +95,9 @@ class Generator:
 
         return zip(review_ids, model_inputs, predictions)
 
-    def _get_output_with_probs(self, batch, forced_decoder_ids=[], current_probability=1.0):
+    def _get_output_with_probs(
+        self, batch, forced_decoder_ids=[], current_probability=1.0
+    ):
         res = [None for _ in range(len(predictions))]
 
         if self.output_probabilities == "best":
@@ -98,74 +106,11 @@ class Generator:
                 for review in outputs["sequences"]
             ]
 
-            # Use softmax to map to actual scores
-            probs = [f.softmax(scores, dim=1) for scores in outputs["scores"]]
+    def _get_output_with_probs(self, batch, generation_canidate: tuple[float, dict]):
+        current_probability, canidate_info = generation_canidate
+        forced_decoder_ids = canidate_info["forced_decoder_ids"]
+        sequence_length = canidate_info["sequence_length"]
 
-            token_probs = [
-                torch.FloatTensor(
-                    [
-                        probs[token_number][
-                            review_number * self.generation_config["num_beams"]
-                        ][
-                            token
-                        ]  # here we choose review_number * num_beams because we only want the first beam which is the best one for each review
-                        for token_number, token in enumerate(review[1:])
-                    ]
-                )
-                for review_number, review in enumerate(outputs["sequences"])
-            ]
-
-            # outputs["sequences"] contains a lot of pad tokens, depending on the longest prediction in the batch (all predictions have equal length). We slice
-            # predicted_tokens and token_probs as soon as we see a pad token to only log relevant tokens
-            for i in range(len(predicted_tokens)):
-                for j in range(len(predicted_tokens[i])):
-                    if predicted_tokens[i][j] == "<pad>":
-                        predicted_tokens[i] = predicted_tokens[i][:j]
-                        token_probs[i] = token_probs[i][:j]
-                        break
-
-            res = zip(
-                [
-                    float(torch.prod(token_probs[i]))
-                    for i in range(len(predicted_tokens))
-                ],
-                [len(predicted_tokens[i]) for i in range(len(predicted_tokens))],
-                [
-                    float(a ** (1 / b))
-                    for a, b in zip(
-                        [
-                            torch.prod(token_probs[i])
-                            for i in range(len(predicted_tokens))
-                        ],
-                        [
-                            len(predicted_tokens[i])
-                            for i in range(len(predicted_tokens))
-                        ],
-                    )
-                ],
-                [
-                    self.tokenizer.decode(self.get_elements_until_zero(review[1:]))
-                    for review in outputs["sequences"]
-                ],
-            )
-
-            res = [
-                [
-                    {
-                        "probability": x[0],
-                        "token_length": x[1],
-                        "geometric_mean_probability": x[2],
-                        "prediction": x[3],
-                    }
-                ]
-                for x in res
-            ]
-
-        return zip(review_ids, model_inputs, predictions, res)
-
-    def _get_output_with_probs(
-        self, batch, forced_decoder_ids=[], current_probability=1.0, token_length=0
-    ):
         num_token_options = 5
         max_new_tokens = len(forced_decoder_ids) + 1
 
@@ -180,85 +125,70 @@ class Generator:
                 output_scores=True,
                 max_new_tokens=max_new_tokens,
                 forced_decoder_ids=forced_decoder_ids,
-                length_penalty=1.5,
             )
         # print(output)
 
-        probs = f.softmax(output["scores"][-1][0], dim=-1)
-        # print(probs)
-        best_token_ids = torch.argsort(probs, descending=True)[:num_token_options]
-        # print(best_token_ids)
-        i = 0
-        cumul_prob = 0
-        for token_id in best_token_ids:
-            cumul_prob += probs[token_id]
-            i += 1
-            if cumul_prob > 0.99:
-                break
+        token_probs = f.softmax(output["scores"][-1][0], dim=0).log()
+        top_k_token_ids, top_k_token_probs = torch.topk(
+            token_probs, k=num_token_options, dim=0
+        )
 
-        for token_id in best_token_ids[:i]:
-            yield {
-                "forced_decoder_ids": forced_decoder_ids
-                + [(max_new_tokens, int(token_id))],
-                "current_probability": current_probability * probs[token_id],
-            }
-                "token_length": token_length + 1,
-            }
+        for token_id in top_k_token_ids:
+            yield (
+                current_probability + top_k_token_probs[token_id],
+                {
+                    "forced_decoder_ids": forced_decoder_ids
+                    + [(max_new_tokens, int(token_id))],
+                    "sequence_length": sequence_length + 1,
+                },
+            )
 
     def generate_usage_options_prob_based(self, batch) -> None:
-        review_ids = list(batch["review_id"])
-        input_ids = batch["input"]["input_ids"].to(self.device)
-        attention_mask = batch["input"]["attention_mask"].to(self.device)
-
-        model_inputs = self.tokenizer.batch_decode(input_ids, skip_special_tokens=True)
-
-        generation_queue = [{"forced_decoder_ids": [], "current_probability": 1.0}]
         results = []
-        while generation_queue:
+
+        total_probability = 0
+        MAX_ITERATIONS = 200
+
+        generation_queue = PriorityQueue()
+
+        generation_queue.put((0, {"forced_decoder_ids": [], "token_length": 0}))
+
+        i = 0
+        while (
+            not generation_queue.empty()
+            and i < MAX_ITERATIONS
+            and total_probability <= 0.95
+        ):
             next_generations = self._get_output_with_probs(
-                batch=batch, **generation_queue.pop(0)
+                batch, generation_queue.get()
             )
-            # print(list(next_generations))
+            i += 1
 
             for generation in next_generations:
-                if generation["forced_decoder_ids"][-1][1] == 1:
+                if generation[1]["forced_decoder_ids"][-1][1] == 1:
+                    total_probability += exp(-generation[0])
                     results.append(generation)
-                    continue
-                if generation["current_probability"] > 0.001:
-                    generation_queue.append(generation)
-
-        print("results:", results)
-        print("generation_queu:e", generation_queue)
+                else:
+                    generation_queue.put(generation)
 
         decoded_results = self.tokenizer.batch_decode(
             [
-                [token_id for _, token_id in result["forced_decoder_ids"]]
+                [token_id for _, token_id in result[1]["forced_decoder_ids"]]
                 for result in results
             ]
         )
         decoded_results_with_probs = [
-            (float(result["current_probability"]), output)
+            (
+                exp(
+                    -float(result[0])
+                ),  # casting current_probability to float because it is a tensor
+                result[1]["sequence_length"],
+                output,
+            )
             for result, output in zip(results, decoded_results)
-        ]
-        print(model_inputs)
-        decoded_results_with_probs = sorted(
-            decoded_results_with_probs, key=lambda x: x[0], reverse=True
-        )
-        for prob, output in decoded_results_with_probs:
-            print(prob, output)
-        sum_of_probs = sum([prob for prob, _ in decoded_results_with_probs])
-        print(sum_of_probs)
+        ]  # is a list with tuples (probability, length, prediction)
 
-        print("------------------")
-
-        # with torch.no_grad():
-        #     outputs = self.model.generate(
-        #         input_ids=input_ids,
-        #         attention_mask=attention_mask,
-        #         max_new_tokens=1,
-        #         min_new_tokens=1,
-        #         forced_decoder_ids=decoder_ids,
-        #     )
+        return decoded_results_with_probs
 
     def generate_label(
         self, reviews: ReviewSet, label_id: str = None, verbose: bool = False
@@ -299,7 +229,6 @@ class Generator:
             print(f"Label Metadata (base): {label_metadata}", end="\n\n")
 
         for batch in dataloader:
-
             if self.output_probabilities == "all":
                 usage_options_batch = list(
                     self.generate_usage_options_prob_based(batch)
