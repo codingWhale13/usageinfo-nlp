3488bac82df38821f6a0fe7c243c69d34850edc1
matthis.clausen <matthis.clausen@student.hpi.uni-potsdam.de>
2023-06-22 09:50:32 +0000

    Cluster labeling [feat]: clear terminal and invalid input handling

--- experiments/cluster_labeling.py
+++ experiments/cluster_labeling.py
@@ -1,31 +1,34 @@
 import csv
 import pandas as pd
+import os
 
 pairs = [("a", "b"), ("c", "d"), ("e", "f")]
 current_pair_index = 0
 results = []
 
 
+def instructions():
+    clear_terminal()
+    print("This script will show you pairs of usage options.")
+    print("Press 'y' if you think they should appear in the same cluster.")
+    print("Press 'n' if the usage options are not similar at all.")
+    print("Press enter to continue")
+    vote = input()
+    if vote != "":
+        exit()
+
+
 def get_next_pair():
     global pairs
     global current_pair_index
     if current_pair_index < len(pairs):
-        print(f"Pair: {pairs[current_pair_index]}")
+        pair = pairs[current_pair_index]
         current_pair_index += 1
+        return pair
     else:
         save_results()
         print("Labeling finished")
         exit()
-
-
-def vote_similar():
-    record_vote(True)
-    get_next_pair()
-
-
-def vote_not_similar():
-    record_vote(False)
-    get_next_pair()
 
 
 def record_vote(similar):
@@ -40,19 +43,34 @@ def save_results():
         writer.writerows(results)
 
 
+def clear_terminal():
+    if os.name == "nt":  # for Windows
+        os.system("cls")
+    else:  # for Linux and Mac
+        os.system("clear")
+
+
 def main():
-    get_next_pair()
+    instructions()
     while True:
-        print(
-            "Would you consider these usage options to appear in the same cluster? (y/n)"
-        )
-        vote = input()
-        if vote == "y":
-            vote_similar()
-        elif vote == "n":
-            vote_not_similar()
-        else:
-            print("Invalid input")
+        clear_terminal()
+        pair = get_next_pair()
+        while True:
+            print(f"Usage option 1: {pair[0]}")
+            print(f"Usage option 2: {pair[1]}")
+            print(
+                "Do these usage options describe the same usage option and should therefore appear in the same cluster? (y/n)"
+            )
+            vote = input()
+
+            if vote == "y":
+                record_vote(True)
+                break
+            elif vote == "n":
+                record_vote(False)
+                break
+            else:
+                print("Invalid input")
 
 
 if __name__ == "__main__":
