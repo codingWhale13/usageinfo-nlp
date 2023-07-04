c88a0bc5f31131caf355d55d9ffb7a3633ca6244
Nils Kiele <nils.kiele@student.hpi.uni-potsdam.de>
2023-07-04 14:13:10 +0200

    [nk/json-split]: Add script to split a JSON file into many, with one label each
    * helpers [feat]: add script to split a JSON file into many, with one label each
    
    
    Merged by: Frederic Sadrieh <frederic.sadrieh@student.hpi.uni-potsdam.de>
    Co-authored-by: codingWhale13 <nils.kiele@gmail.com>

---
+++ helpers/split_jsons_by_label_id.py
@@ -0,0 +1,29 @@
+import os
+from copy import deepcopy
+from helpers.review_set import ReviewSet
+
+# This script takes a JSON file with many labels and splits it into one file per label.
+
+ALL_LABELS_PATH = "data/ba/ba-30k-test-all.json"
+SINGLE_LABELS_FOLDER = "data/ba/single_label"
+
+large_review_set = ReviewSet.from_files(ALL_LABELS_PATH)
+
+label_ids = large_review_set.get_all_label_ids()
+single_review_sets = {
+    label_id: ReviewSet(version=5, reviews={}) for label_id in label_ids
+}
+
+for review_id, review in large_review_set.items():
+    for label_id in label_ids:
+        if label_id in review.data["labels"]:
+            review_copy = deepcopy(review)
+            review_copy.data["labels"] = {
+                label_id: review_copy.data["labels"][label_id]
+            }
+            single_review_sets[label_id].reviews[review_id] = review_copy
+
+for label_id in label_ids:
+    single_review_sets[label_id].save(
+        os.path.join(SINGLE_LABELS_FOLDER, f"{label_id}.json")
+    )
